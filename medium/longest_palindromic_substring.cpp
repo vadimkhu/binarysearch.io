35064d78e03b41adb282bb6cc472dc5bf91b8ff6
Vadim Khudozhilov <ccod@mail.ru>
2021-01-20 10:35:51 +0300

    Create longest_palindromic_substring.cpp

---
+++ medium/longest_palindromic_substring.cpp
@@ -0,0 +1,40 @@
+/*
+  Longest Palindromic Substring
+  Question 119
+  
+  https://binarysearch.com/problems/Longest-Palindromic-Substring
+  
+  Solution:
+  https://www.geeksforgeeks.org/longest-palindrome-substring-set-1/
+*/
+
+int solve(string s) {
+    int n = s.size();
+    vector<vector<bool>> dp(n, vector<bool>(n, 0));
+    int maxLength = 1;
+    for (int i = 0; i < n; ++i)
+        dp[i][i] = true;
+ 
+    int start = 0;
+    for (int i = 0; i < n - 1; ++i) {
+        if (s[i] == s[i + 1]) {
+            dp[i][i + 1] = true;
+            start = i;
+            maxLength = 2;
+        }
+    }
+ 
+    for (int k = 3; k <= n; ++k) {
+        for (int i = 0; i < n - k + 1; ++i) {
+            int j = i + k - 1;
+            if (dp[i + 1][j - 1] && s[i] == s[j]) {
+                dp[i][j] = true;
+                if (k > maxLength) {
+                    start = i;
+                    maxLength = k;
+                }
+            }
+        }
+    }
+    return maxLength;    
+}
